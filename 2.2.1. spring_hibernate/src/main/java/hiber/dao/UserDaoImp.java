4a7bd2abb6557bd3d9f88cb315b8bba4163dac7a
VisCram <126577181+VisCram@users.noreply.github.com>
2023-09-11 18:16:55 +0300

    add a car to users

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,4 @@
 package hiber.dao;
-
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
-
 @Repository
 public class UserDaoImp implements UserDao {
 
@@ -19,11 +17,19 @@ public class UserDaoImp implements UserDao {
       sessionFactory.getCurrentSession().save(user);
    }
 
+
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
-
+   @Override
+   public User getUserByCar(String model,int series) {
+      TypedQuery <User> query =  sessionFactory.getCurrentSession().createQuery(
+              "from User user where user.car.model=:model and user.car.series=:series",User.class);
+      query.setParameter("model",model);
+      query.setParameter("series",series);
+      return query.getSingleResult();
+   }
 }
